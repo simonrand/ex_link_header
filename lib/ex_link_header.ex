f3a355814a193b36dbee68895073e2b8be62018c
Simon Rand <simonrand@gmail.com>
2016-01-17 22:21:12 +0000

    Don't include links with no relationship param

--- lib/ex_link_header.ex
+++ lib/ex_link_header.ex
@@ -18,10 +18,18 @@ defmodule ExLinkHeader do
     parse_links(links)
   end
 
+  defp is_rel?(name) do
+    name == "rel"
+  end
+
   defp parse_links(links) do
 
-    parsed_links = Enum.map(links, fn link ->
-        [_, url, name] = Regex.run(~r{<([^>]+)>; rel="?([a-z]+)"?}, link)
+    parsed_links = Enum.filter_map(links, fn link ->
+      [_, param] = Regex.run(~r{<[^>]+>; (\w+)="?[a-z]+"?.+}, link)
+      is_rel?(param)
+    end, fn link ->
+        [_, url, name] = Regex.run(~r{<([^>]+)>; \w+="?([a-z]+)"?}, link)
+
         page = case Regex.run(~r{.+[\?|\&]page=(\d+)\??}, url) do
           [_, page] -> page
           nil -> nil
@@ -32,8 +40,9 @@ defmodule ExLinkHeader do
           nil -> nil
         end
 
-        {name, %{ url: url, page: page, per_page: per_page }}
-      end)
+        {name, %{ url: url, page: page, per_page: per_page, rel: name }}
+      end
+    )
 
     parsed_links
     |> Enum.into(%{})
--- test/ex_link_header_test.exs
+++ test/ex_link_header_test.exs
@@ -15,27 +15,14 @@ defmodule ExLinkHeaderTest do
       %{"next" => %{
           url: "https://api.github.com/user/simonrand/repos?per_page=100&page=2",
           page: "2",
-          per_page: "100"
+          per_page: "100",
+          rel: "next"
         },
         "last" => %{
           url: "https://api.github.com/user/simonrand/repos?page=3&per_page=100",
           page: "3",
-          per_page: "100"
-        }
-      }
-  end
-
-  test "parsing a header with a comma in a link" do
-    link_header =
-      "<https://api.github.com/search/repositories?q=elixir,ruby&sort=stars&order=desc>; rel=\"last\""
-
-    links = ExLinkHeader.parse(link_header)
-
-    assert links ==
-      %{"last" => %{
-          url: "https://api.github.com/search/repositories?q=elixir,ruby&sort=stars&order=desc",
-          page: nil,
-          per_page: nil
+          per_page: "100",
+          rel: "last"
         }
       }
   end
@@ -51,12 +38,70 @@ defmodule ExLinkHeaderTest do
       %{"next" => %{
           url: "https://api.github.com/user/simonrand/repos?per_page=100&page=2",
           page: "2",
-          per_page: "100"
+          per_page: "100",
+          rel: "next"
         },
         "last" => %{
           url: "https://api.github.com/user/simonrand/repos?page=3&per_page=100",
           page: "3",
-          per_page: "100"
+          per_page: "100",
+          rel: "last"
+        }
+      }
+  end
+
+  test "parsing a header with next and last links and only some url params" do
+    link_header =
+      "<https://api.github.com/user/simonrand/repos?per_page=100>; rel=\"next\", " <>
+      "<https://api.github.com/user/simonrand/repos?page=3>; rel=\"last\""
+
+    links = ExLinkHeader.parse(link_header)
+
+    assert links ==
+      %{"next" => %{
+          url: "https://api.github.com/user/simonrand/repos?per_page=100",
+          page: nil,
+          per_page: "100",
+          rel: "next"
+        },
+        "last" => %{
+          url: "https://api.github.com/user/simonrand/repos?page=3",
+          page: "3",
+          per_page: nil,
+          rel: "last"
+        }
+      }
+  end
+
+  test "parsing a header including a link without a relationship param" do
+    link_header =
+      "<https://api.github.com/user/simonrand/repos?per_page=100&page=2>; rel=next, " <>
+      "<https://api.github.com/user/simonrand/repos?page=3&per_page=100>; ler=last"
+
+    links = ExLinkHeader.parse(link_header)
+
+    assert links ==
+      %{"next" => %{
+          url: "https://api.github.com/user/simonrand/repos?per_page=100&page=2",
+          page: "2",
+          per_page: "100",
+          rel: "next"
+        }
+      }
+  end
+
+  test "parsing a header with a comma in a link" do
+    link_header =
+      "<https://api.github.com/search/repositories?q=elixir,ruby&sort=stars&order=desc>; rel=\"last\""
+
+    links = ExLinkHeader.parse(link_header)
+
+    assert links ==
+      %{"last" => %{
+          url: "https://api.github.com/search/repositories?q=elixir,ruby&sort=stars&order=desc",
+          page: nil,
+          per_page: nil,
+          rel: "last"
         }
       }
   end
